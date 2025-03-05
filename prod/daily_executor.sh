b127421d54e51715d532d37c821dca24981049db
Ubuntu <ubuntu@ip-172-31-39-210.eu-north-1.compute.internal>
2025-03-05 15:18:06 +0000

    first setup

--- prod/daily_executor.sh
+++ prod/daily_executor.sh
@@ -1,5 +1,5 @@
 # Run docker compose and log output with timestamp
-docker compose up -f "~/finance/stock-analyser-deploy/develop/docker-compose" 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
+docker compose up -f "~/finance/stock-analyser-deploy/prod/docker-compose.yml" 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
 
 # Get the instance ID dynamically
 INSTANCE_ID=$(ec2-metadata -i | awk '{print $2}')
