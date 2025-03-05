910f616edb172a8363223fcef9b93ee4748bce46
Ubuntu <ubuntu@ip-172-31-39-210.eu-north-1.compute.internal>
2025-03-05 14:45:45 +0000

    first setup

---
+++ prod/daily_executor.sh
@@ -0,0 +1,7 @@
+# Run docker compose and log output with timestamp
+docker compose up -f "~/finance/stock-analyser-deploy/develop/docker-compose" 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
+
+# Get the instance ID dynamically
+INSTANCE_ID=$(ec2-metadata -i | awk '{print $2}')
+
+aws ec2 stop-instances --instance-ids $INSTANCE_ID
