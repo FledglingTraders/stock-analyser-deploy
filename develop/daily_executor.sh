e59b2500d0dd2462265ad9788bdb4cf4844bd221
Ubuntu <ubuntu@ip-172-31-39-210.eu-north-1.compute.internal>
2025-03-12 12:43:04 +0000

    add daily_executor.sh file

--- develop/daily_executor.sh
+++ develop/daily_executor.sh
@@ -1,5 +1,7 @@
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 108782078484.dkr.ecr.eu-north-1.amazonaws.com
+
 # Run docker compose and log output with timestamp
-docker compose up -f "~/finance/stock-analyser-deploy/develop/docker-compose.yml" 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
+docker compose -f ~/finance/stock-analyser-deploy/develop/docker-compose.yml up 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
 
 # Get the instance ID dynamically
 INSTANCE_ID=$(ec2-metadata -i | awk '{print $2}')
