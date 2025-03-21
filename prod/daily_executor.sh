21debee09aa5aefc1261c38ed816cbe7cba141c6
Ubuntu <ubuntu@ip-172-31-39-210.eu-north-1.compute.internal>
2025-03-21 00:26:29 +0000

    update script

--- prod/daily_executor.sh
+++ prod/daily_executor.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
 git pull
+
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 108782078484.dkr.ecr.eu-north-1.amazonaws.com
 
-# Run docker compose and log output with timestamp
-docker compose -f ~/finance/stock-analyser-deploy/prod/docker-compose.yml up 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
+# Ensure logs directory exists
+mkdir -p logs
+
+# Run docker compose and log output with timestamp don't log in terminal
+# docker compose -f ~/finance/stock-analyser-deploy/develop/docker-compose.yml up 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
+
+# Run docker compose, log output to both terminal and file
+docker compose -f ~/finance/stock-analyser-deploy/prod/docker-compose.yml up 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' | tee -a "logs/logs_$(date '+%Y-%m-%d_%H-%M').log"
 
 # Get the instance ID dynamically
 INSTANCE_ID=$(ec2-metadata -i | awk '{print $2}')
 
-# aws ec2 stop-instances --instance-ids $INSTANCE_ID
+# aws ec2 stop-instances --instance-ids
